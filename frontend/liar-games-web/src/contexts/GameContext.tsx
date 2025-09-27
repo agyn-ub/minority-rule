7ef971d040305c9cb4ff7e322e1163c94eb79f40
agyn-ub <agyn.bolatov.1992@gmail.com>
2025-09-27 22:52:14 +0530

    submit vote

--- frontend/liar-games-web/src/contexts/GameContext.tsx
+++ frontend/liar-games-web/src/contexts/GameContext.tsx
@@ -375,8 +375,21 @@ export function GameProvider({ children }: { children: React.ReactNode }) {
           import MinorityRuleGame from 0xMinorityRuleGame
 
           transaction(gameId: UInt64, vote: Bool) {
+            let playerAddress: Address
+
             prepare(signer: auth(BorrowValue) &Account) {
-              MinorityRuleGame.submitVote(gameId: gameId, player: signer.address, vote: vote)
+              self.playerAddress = signer.address
+            }
+
+            execute {
+              // Get reference to the game
+              let game = MinorityRuleGame.borrowGame(gameId)
+                  ?? panic("Game does not exist")
+
+              // Submit the vote
+              game.submitVote(player: self.playerAddress, vote: vote)
+
+              log("Player ".concat(self.playerAddress.toString()).concat(" submitted vote for game ").concat(gameId.toString()))
             }
           }
         `,
